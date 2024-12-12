0aca3917830b34ac079fd9e6ffc46f655ef8e3a8
copypasting-warrior <baruaharyan251@gmail.com>
2024-12-12 15:28:07 +0530

    fixed layout error in add item

--- src/app/adminPages/addInventory/page.tsx
+++ src/app/adminPages/addInventory/page.tsx
@@ -1272,7 +1272,7 @@ const AddInventory = () => {
       <div className="flex flex-col gap-9 overflow-x-hidden ">
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
           <form onSubmit={handleInventorySubmit}>
-            <div className="md:grid md:grid-cols-1 gap-6 p-6.5 flex flex-col" >
+            <div className="md:grid md:grid-cols-3 gap-6 p-6.5 flex flex-col" >
               {/* Item ID */}
               <div>
                 <label className="mb-3 block text-sm font-medium text-black dark:text-white">
